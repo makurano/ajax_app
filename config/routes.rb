864548ebd4fc8491243f32bac078547a7ba0a236
makurano <ginsyari1@gmail.com>
2020-12-01 11:48:28 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
+  # リソースを識別したいのでpathパラメーターでエンドポイントを作る
+  get 'posts/:id', to: 'posts#checked'
 end
